475a8838dc0b709dc18d3f9ebc076c8a0cfec6bc
sandeep shiven <sandeepshiven0@gmail.com>
2019-12-14 19:05:30 +0530

    added project model in jobs app

--- jobs/admin.py
+++ jobs/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Job
+from .models import Project
 
 admin.site.register(Job)
+admin.site.register(Project)
--- jobs/models.py
+++ jobs/models.py
@@ -4,3 +4,7 @@ class Job(models.Model):
     image = models.ImageField(upload_to='images/')
     description = models.CharField(max_length=300)
 
+class Project(models.Model):
+    image = models.ImageField(upload_to='images/')
+    description = models.CharField(max_length=300)
+    github_link = models.URLField()
