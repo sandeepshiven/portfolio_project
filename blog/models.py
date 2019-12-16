a402c08cac6a8c6be7705de45d6570d1045c71f1
sandeep shiven <sandeepshiven0@gmail.com>
2019-12-16 17:45:48 +0530

    added all blogs page to show all of the blogs

--- blog/models.py
+++ blog/models.py
@@ -8,6 +8,12 @@ class Blog(models.Model):
     body = models.TextField()
     image = models.ImageField(upload_to = 'images/')
 
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:200]+"................."
+
 
 
 
